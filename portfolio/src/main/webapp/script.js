7b59cf1b62c44e2fd3c76b682337ffe262559bf7
Omar Montiel <omarume@gmail.com>
2020-06-26 19:51:29 +0000

    Fix indentation applying spaces instead of tabs

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -18,23 +18,23 @@
 
 let bibleVerses = new Map();
 bibleVerses.set('Romans 5: 3-5', 'Not only so, but we also glory in our ' +
-		'sufferings, because we know that suffering produces perseverance; ' +
-		'perseverance, character; and character, hope. And hope does not put us ' +
-		'to shame, because God’s love has been poured out into our hearts ' +
-		'through the Holy Spirit, who has been given to us.');
+    'sufferings, because we know that suffering produces perseverance; ' +
+    'perseverance, character; and character, hope. And hope does not put us ' +
+    'to shame, because God’s love has been poured out into our hearts ' +
+    'through the Holy Spirit, who has been given to us.');
 bibleVerses.set('Romans 10:13', 'For, “Everyone who calls on the name of ' +
-		'the Lord will be saved.”');
+    'the Lord will be saved.”');
 bibleVerses.set('Psalms 94:19', 'When anxiety was great within me, your ' +
-		'consolation brought me joy.');
+    'consolation brought me joy.');
 bibleVerses.set('John 3:16', 'For God so loved the world that he gave his ' +
-		'one and only Son, that whoever believes in him shall not perish but ' +
-		'have eternal life.');
+    'one and only Son, that whoever believes in him shall not perish but ' +
+    'have eternal life.');
 bibleVerses.set('James 1:5', 'If any of you lacks wisdom, you should ask ' +
-		'God, who gives generously to all without finding fault, and it will be ' +
-		'given to you.');
+    'God, who gives generously to all without finding fault, and it will be ' +
+    'given to you.');
 bibleVerses.set('Joshua 1:9', 'Have I not commanded you? Be strong and ' +
-		'courageous. Do not be afraid; do not be discouraged, for the Lord your ' +
-		'God will be with you wherever you go.');
+    'courageous. Do not be afraid; do not be discouraged, for the Lord your ' +
+    'God will be with you wherever you go.');
 
 /**
  * Select a random key from bibleVerses map and return and array with
@@ -42,21 +42,21 @@ bibleVerses.set('Joshua 1:9', 'Have I not commanded you? Be strong and ' +
  * @return {!ARRAY<string>}
  */
 function getRandomBibleVerse() {
-	// Pick random index
+  // Pick random index
   const bibleVerseIndex = Math.floor(Math.random() * bibleVerses.size);
 	
-	// Select index from map
-	let index = 0;
-	let text = '';
-	let verse = '';
-	for(let key of bibleVerses.keys()) {
-		if(index++ === bibleVerseIndex) {
-			text = bibleVerses.get(key);
-			verse = key;
-		}
-	}
-	const bibleVerse = [text, verse];
-	return bibleVerse;
+  // Select index from map
+  let index = 0;
+  let text = '';
+  let verse = '';
+  for(let key of bibleVerses.keys()) {
+    if(index++ === bibleVerseIndex) {
+      text = bibleVerses.get(key);
+      verse = key;
+    }
+  }
+  const bibleVerse = [text, verse];
+  return bibleVerse;
 }
 
 /**
@@ -64,13 +64,13 @@ function getRandomBibleVerse() {
  */
 function addRandomBibleVerse() {
   // Pick a random Bible verse.
-	const bibleVerse = getRandomBibleVerse();
+  const bibleVerse = getRandomBibleVerse();
 
   // Add it to the page.
   const bibleVerseTextContainer = 
-			document.getElementById('bible-verse-text-container');
+      document.getElementById('bible-verse-text-container');
   bibleVerseTextContainer.innerText = bibleVerse[0];
   const bibleVerseContainer = 
-			document.getElementById('bible-verse-container');
+	    document.getElementById('bible-verse-container');
   bibleVerseContainer.innerText = bibleVerse[1];
 }
