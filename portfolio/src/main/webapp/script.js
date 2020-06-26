b819d2513839826cc0d5af26f105480fe94dcddf
Omar Montiel <omarume@gmail.com>
2020-06-26 18:44:28 +0000

    Change Bible verses arrays to maps and construct them outside the function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,33 +13,56 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a random Bible verse to the page.
  */
+
+let bibleVerses = new Map();
+bibleVerses.set('Romans 5: 3-5', 'Not only so, but we also glory in our ' +
+		'sufferings, because we know that suffering produces perseverance; ' +
+		'perseverance, character; and character, hope. And hope does not put us ' +
+		'to shame, because God’s love has been poured out into our hearts ' +
+		'through the Holy Spirit, who has been given to us.');
+bibleVerses.set('Romans 10:13', 'For, “Everyone who calls on the name of ' +
+		'the Lord will be saved.”');
+bibleVerses.set('Psalms 94:19', 'When anxiety was great within me, your ' +
+		'consolation brought me joy.');
+bibleVerses.set('John 3:16', 'For God so loved the world that he gave his ' +
+		'one and only Son, that whoever believes in him shall not perish but ' +
+		'have eternal life.');
+bibleVerses.set('James 1:5', 'If any of you lacks wisdom, you should ask ' +
+		'God, who gives generously to all without finding fault, and it will be ' +
+		'given to you.');
+bibleVerses.set('Joshua 1:9', 'Have I not commanded you? Be strong and ' +
+		'courageous. Do not be afraid; do not be discouraged, for the Lord your ' +
+		'God will be with you wherever you go.');
+
+function getRandomBibleVerse() {
+	// Pick random index
+  const bibleVerseIndex = Math.floor(Math.random() * bibleVerses.size);
+	
+	// Select index from map
+	let index = 0;
+	let text = '';
+	let verse = '';
+	for(let key of bibleVerses.keys()) {
+		if(index++ === bibleVerseIndex) {
+			text = bibleVerses.get(key);
+			verse = key;
+		}
+	}
+	const bibleVerse = [text, verse];
+	return bibleVerse;
+}
+
 function addRandomBibleVerse() {
-  const bibleVersesText =
-      ['Not only so, but we also glory in our sufferings, because we know that suffering produces perseverance; perseverance, character; and character, hope. And hope does not put us to shame, because God’s love has been poured out into our hearts through the Holy Spirit, who has been given to us.',
-      'For, “Everyone who calls on the name of the Lord will be saved.”',
-      'When anxiety was great within me, your consolation brought me joy.',
-      'For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.',
-      'If any of you lacks wisdom, you should ask God, who gives generously to all without finding fault, and it will be given to you.',
-      'Have I not commanded you? Be strong and courageous. Do not be afraid; do not be discouraged, for the Lord your God will be with you wherever you go.'];
-
-  const bibleVerses = 
-      ['Romans 5: 3-5',
-      'Romans 10:13',
-      'Psalms 94:19',
-      'John 3:16',
-      'James 1:5',
-      'Joshua 1:9']
-
-  // Pick a random greeting.
-  const bibleVerseIndex = Math.floor(Math.random() * bibleVersesText.length);
-  const bibleVerseText = bibleVersesText[bibleVerseIndex];
-  const bibleVerse = bibleVerses[bibleVerseIndex];
+  // Pick a random Bible verse.
+	const bibleVerse = getRandomBibleVerse();
 
   // Add it to the page.
-  const bibleVerseTextContainer = document.getElementById('bible-verse-text-container');
-  bibleVerseTextContainer.innerText = bibleVerseText;
-  const bibleVerseContainer = document.getElementById('bible-verse-container');
-  bibleVerseContainer.innerText = bibleVerse;
+  const bibleVerseTextContainer = 
+			document.getElementById('bible-verse-text-container');
+  bibleVerseTextContainer.innerText = bibleVerse[0];
+  const bibleVerseContainer = 
+			document.getElementById('bible-verse-container');
+  bibleVerseContainer.innerText = bibleVerse[1];
 }
