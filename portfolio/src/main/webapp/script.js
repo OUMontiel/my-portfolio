9d6dcedc32bf7d947b4cb8835772102badc21db6
Omar Montiel <omarume@gmail.com>
2020-06-27 00:55:00 +0000

    Fix indentation and comments and change 'let' variable to 'const'

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,42 +15,42 @@
 /**
  * Adds a random Bible verse to the page.
  */
-let bibleVerses = new Map();
-bibleVerses.set('Romans 5: 3-5', 'Not only so, but we also glory in our ' +
+const BIBLE_VERSES = new Map();
+BIBLE_VERSES.set('Romans 5: 3-5', 'Not only so, but we also glory in our ' +
     'sufferings, because we know that suffering produces perseverance; ' +
     'perseverance, character; and character, hope. And hope does not put us ' +
     'to shame, because God’s love has been poured out into our hearts ' +
     'through the Holy Spirit, who has been given to us.');
-bibleVerses.set('Romans 10:13', 'For, “Everyone who calls on the name of ' +
+BIBLE_VERSES.set('Romans 10:13', 'For, “Everyone who calls on the name of ' +
     'the Lord will be saved.”');
-bibleVerses.set('Psalms 94:19', 'When anxiety was great within me, your ' +
+BIBLE_VERSES.set('Psalms 94:19', 'When anxiety was great within me, your ' +
     'consolation brought me joy.');
-bibleVerses.set('John 3:16', 'For God so loved the world that he gave his ' +
+BIBLE_VERSES.set('John 3:16', 'For God so loved the world that he gave his ' +
     'one and only Son, that whoever believes in him shall not perish but ' +
     'have eternal life.');
-bibleVerses.set('James 1:5', 'If any of you lacks wisdom, you should ask ' +
+BIBLE_VERSES.set('James 1:5', 'If any of you lacks wisdom, you should ask ' +
     'God, who gives generously to all without finding fault, and it will be ' +
     'given to you.');
-bibleVerses.set('Joshua 1:9', 'Have I not commanded you? Be strong and ' +
+BIBLE_VERSES.set('Joshua 1:9', 'Have I not commanded you? Be strong and ' +
     'courageous. Do not be afraid; do not be discouraged, for the Lord your ' +
     'God will be with you wherever you go.');
 
 /**
- * Select a random key from bibleVerses map and return and array with
+ * Select a random key from BIBLE_VERSES map and return an array with
  * the key and its value.
  * @return {!ARRAY<string>}
  */
 function getRandomBibleVerse() {
   // Pick random index
-  const bibleVerseIndex = Math.floor(Math.random() * bibleVerses.size);
+  const bibleVerseIndex = Math.floor(Math.random() * BIBLE_VERSES.size);
 	
   // Select index from map
   let index = 0;
   let text = '';
   let verse = '';
-  for(let key of bibleVerses.keys()) {
+  for(let key of BIBLE_VERSES.keys()) {
     if(index++ === bibleVerseIndex) {
-      text = bibleVerses.get(key);
+      text = BIBLE_VERSES.get(key);
       verse = key;
     }
   }
@@ -70,7 +70,7 @@ function addRandomBibleVerse() {
       document.getElementById('bible-verse-text-container');
   bibleVerseTextContainer.innerText = bibleVerse[0];
   const bibleVerseContainer = 
-	    document.getElementById('bible-verse-container');
+      document.getElementById('bible-verse-container');
   bibleVerseContainer.innerText = bibleVerse[1];
 }
 
