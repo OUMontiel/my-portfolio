5fe31faaba8cbc32f9d7364f57d1f3455be859cb
Omar Montiel <omarume@gmail.com>
2020-07-06 18:28:41 +0000

    Fix end of file, imports order and implementation comments

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -18,11 +18,11 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
 import com.google.gson.Gson;
 import com.google.sps.data.Comment;
 import java.io.IOException;
@@ -43,15 +43,13 @@ public class DataServlet extends HttpServlet {
    */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    /** Create a query and prepare it with the data stored in Datastore. */
+    //Create a query and prepare it with the data stored in Datastore.
     Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
 
-    /**
-     * Add all queried comments from Datastore
-     * to an List of type Comment.
-     */
+    //Add all queried comments from Datastore
+    //to an List of type Comment.
     List<Comment> comments = new ArrayList<>();
     for (Entity entity : results.asIterable()) {
       long id = entity.getKey().getId();
@@ -62,7 +60,7 @@ public class DataServlet extends HttpServlet {
       comments.add(comment);
     }
     
-    /** Convert the comments List to JSON format. */
+    //Convert the comments List to JSON format.
     Gson gson = new Gson();
 
     response.setContentType("text/html;");
@@ -72,21 +70,19 @@ public class DataServlet extends HttpServlet {
   /** Posts a comment retrieved from the form input adding it to the messages variable. */
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    /** Get the comment from the form and add it to the array. */
+    //Get the comment from the form and add it to the array.
     String content = request.getParameter("text-input");
     long timestamp = System.currentTimeMillis();
 
-    /**
-     * Create an Entity that holds the comment and the time it was created
-     * and store it in Datastore.
-     */
+    //Create an Entity that holds the comment and the time it was created
+    //and store it in Datastore.
     Entity commentEntity = new Entity("Comment");
     commentEntity.setProperty("content", content);
     commentEntity.setProperty("timestamp", timestamp);
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(commentEntity);
 
-    /** Redirect back to the HTML page. */
+    //Redirect back to the HTML page.
     response.sendRedirect("/index.html");
   }
 }
