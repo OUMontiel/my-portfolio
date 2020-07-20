e47bab81c44e9712a1af2edabfbf4fabf791046b
Omar Montiel <omarume@gmail.com>
2020-07-20 17:53:40 +0000

    Add comments, rearrange if conditions and make other requested changes

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -20,7 +20,21 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Processes a query to find time ranges for a new meeting given the attendees required and other
+ * events of the day.
+ */
 public final class FindMeetingQuery {
+  /**
+   * Finds all the available time slots for a meeting given the time required for the meeting, the
+   * people that need to attend that meeting, and other events scheduled for the day. This
+   * algorithm is O(1) in terms of space complexity, requiring a boolean array of fixed size
+   * (24 * 60). It is O(mn) in time complexity, 'm' being the number of attendees required for the
+   * meeting and 'n' being the events of the day.
+   * @param events all the events scheduled for a given day, with their attendees and time range.
+   * @param request the request for a meeting, with their required attendees and minimum duration.
+   * @return the available time slots during the day where setting the meeting is possible.
+   */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     boolean[] unavailableMinutes = new boolean[24 * 60];
     Collection<String> meetingAttendees = request.getAttendees();
@@ -31,7 +45,7 @@ public final class FindMeetingQuery {
       for (String meetingAttendee : meetingAttendees) {
         if (eventAttendees.contains(meetingAttendee)) {
           TimeRange eventTimeRange = event.getWhen();
-          addEventTimeRange(unavailableMinutes, eventTimeRange);
+          addUnavailableMinutes(unavailableMinutes, eventTimeRange);
           break;
         }
       }
@@ -39,21 +53,6 @@ public final class FindMeetingQuery {
     
     Collection<TimeRange> availableTimeSlots =
         checkForAvailableTimeSlots(unavailableMinutes, meetingDuration);
-
-    /*System.out.println("---> Unavailable Minutes <---");
-    for (int i = 0; i < 24; i++) {
-      System.out.print(i + ": ");
-      for (int j = 0; j < 60; j++) {
-        if (unavailableMinutes[(i * 60) + j]) System.out.print("1");
-        else System.out.print("0");
-      }
-      System.out.println();
-    }
-    System.out.println("---> Time Ranges <---");
-    for (TimeRange timeRange : availableTimeSlots) {
-      System.out.println("Start: " + timeRange.start() + " - End: " + timeRange.end());
-    }
-    System.out.println();*/
 
     return availableTimeSlots;
   }
@@ -63,16 +62,15 @@ public final class FindMeetingQuery {
    * @param unavailableMinutes the minutes where other events are happening.
    * @param eventTimeRange the time range of a specific event to be added to unavailableMinutes.
    */
-  private void addEventTimeRange(boolean[] unavailableTimeSlots, TimeRange eventTimeRange) {
-    int start = eventTimeRange.start();
-    int end = eventTimeRange.end() - 1;
-    for (int i = start; i <= end; i++) {
-      unavailableTimeSlots[i] = true;
+  private void addUnavailableMinutes(boolean[] unavailableMinutes, TimeRange eventTimeRange) {
+    if (eventTimeRange.start() < 0 || eventTimeRange.end() - 1 > 24 * 60 - 1) return;
+    for (int i = eventTimeRange.start(); i <= eventTimeRange.end() - 1; i++) {
+      unavailableMinutes[i] = true;
     }
   }
 
   /**
-   * Create a collection of available time slots with the information of unavailable minutes in the
+   * Creates a collection of available time slots with the information of unavailable minutes in the
    * day and the minimum duration required.
    * @param unavailableMinutes the minutes during the day where there can be no meetings.
    * @param duration the minimum duration required for the requested meeting.
@@ -80,6 +78,7 @@ public final class FindMeetingQuery {
    */
   private Collection<TimeRange> checkForAvailableTimeSlots(
       boolean[] unavailableMinutes, long duration) {
+    // If duration required for the meeting is longer than a day, meeting is not possible.
     if (duration > 24 * 60) {
       return Collections.emptyList();
     }
@@ -89,25 +88,21 @@ public final class FindMeetingQuery {
     int timeRangeStart = 0;
     int timeRangeEnd = 0;
     for (int i = 0; i < unavailableMinutes.length; i++) {
-      // If the current minute is unavailable, check if it is inside a time range. If it is, change
-      // inTimeRange to false add a new time range with the current start and end times to the
-      // collection of available time slots if its duration is greater than the required.
-      if (unavailableMinutes[i]) {
-        if (inTimeRange) {
-          inTimeRange = false;
-          if(timeRangeEnd - timeRangeStart + 1 >= duration) {
-            availableTimeSlots.add(TimeRange.fromStartEnd(timeRangeStart, timeRangeEnd, true));
-          }
-        }
-      // If the current minute is available, check if it is inside a time range. If it is not,
-      // change inTimeRange to true and mark the beginning of that new time range. If it is, update
-      // the end of the current time range.
-      } else {
-        if (!inTimeRange) {
-          inTimeRange = true;
-          timeRangeStart = i;
-        } else {
-          timeRangeEnd = i;
+      // If the current minute is available but not in a time range, start the time range and set
+      // this minute to the start of that time range.
+      if (!unavailableMinutes[i] && !inTimeRange) {
+        inTimeRange = true;
+        timeRangeStart = i;
+      // If the current minute is available and already in a time range, update the end of this
+      // time range to this minute.
+      } else if (!unavailableMinutes[i] && inTimeRange) {
+        timeRangeEnd = i;
+      // If the current minute is unavailable but inside a time range, end the time range and add
+      // it to the list of available time slots if its duration is greater than the required.
+      } else if (inTimeRange) {
+        inTimeRange = false;
+        if(timeRangeEnd - timeRangeStart + 1 >= duration) {
+          availableTimeSlots.add(TimeRange.fromStartEnd(timeRangeStart, timeRangeEnd, true));
         }
       }
     }
